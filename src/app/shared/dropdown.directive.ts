56ad247a20e8780c23685d3426d45efb751c9e58
Radó Péter <rado.peter@dss.hu>
2019-05-22 10:23:27 +0200

    add dropdown directive with hostlistener and hostbinding

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,8 +1,18 @@
-import {Directive} from '@angular/core';
+import {Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDropDown]'
 })
 export class DropdownDirective {
+  @HostBinding('style.font-weight') fontWeight: string;
+
   constructor() {}
+
+  @HostListener('mouseenter') mouseEnter() {
+    this.fontWeight = 'bold';
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.fontWeight = 'normal';
+  }
 }
